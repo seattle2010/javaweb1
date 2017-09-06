d34dbe5d0dc7e7baf2a41e1134c7324e7e08f1c4
seattle2010 <shitn2000@163.com>
2017-09-07 03:03:54 +0800

    0907

---
+++ ServletTrain201709/src/HiServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HiServlet", urlPatterns = "/HiServlet")
+public class HiServlet extends HttpServlet {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        //=============================================
+System.out.println("hello"); 
+    }
+
+}
